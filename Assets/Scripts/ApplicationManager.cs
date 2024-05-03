f04087ae5588b5176e3110b2551a751497854c4c
wangyijenny <52047217+wangyijenny@users.noreply.github.com>
2024-05-02 21:07:26 -0400

    add functionality for arbitrary cubes

--- Assets/Scripts/ApplicationManager.cs
+++ Assets/Scripts/ApplicationManager.cs
@@ -11,14 +11,35 @@ public class ApplicationManager : MonoBehaviour
     [SerializeField] private PositionDataReader _positionDataReader;
     [SerializeField] private CubeMovementController _cubeMovementController;
 
+
+    public List<int> uniqueObjectIDs = new List<int>();
+    public List<List<PositionData>> dataSortedByObject = new List<List<PositionData>>();
+
     private void Start()
     {
         var data = _positionDataReader.ReadPositionData();
         Array.Sort(data, (a, b) => a.RelativeTimestamp.CompareTo(b.RelativeTimestamp));
         //Array.Sort(data, SortPositionData);
 
-        _cubeMovementController.SetPositionData(data);
-        _cubeMovementController.SetMovementEnabled(true);
+        // find the unique cubes in the data and create a list of postions for each cube
+        foreach (PositionData pos in data)
+        {
+            // if the ObjectID is unique, add it to a list of unique ids 
+            // and add a list in dataSortedByObject for it
+            if (!uniqueObjectIDs.Contains( pos.ObjectID))
+            {
+                uniqueObjectIDs.Add(pos.ObjectID);
+                dataSortedByObject.Add(new List<PositionData>());
+            }
+
+            // add the position to the appropriate list
+            int index = uniqueObjectIDs.IndexOf(pos.ObjectID);
+            dataSortedByObject[index].Add(pos);
+        }
+
+       _cubeMovementController.SetPositionData(dataSortedByObject);
+       _cubeMovementController.SetMovementEnabled(true);
+
     }
 
     private int SortPositionData(PositionData a, PositionData b){
--- Assets/Scripts/CubeMovementController.cs
+++ Assets/Scripts/CubeMovementController.cs
@@ -10,14 +10,22 @@ using UnityEngine.Serialization;
 /// </summary>
 public class CubeMovementController : MonoBehaviour
 {
-    [SerializeField] private CubeMovement _cubeMovement;
+    //[SerializeField] private CubeMovement _cubeMovement;
     
     private bool _isMovementEnabled;
+
+    public GameObject cubePrefab;
     
-    public void SetPositionData(PositionData[] positionData)
+    public void SetPositionData(List<List<PositionData>> positionData)
     {
-        var positionQueue = positionData != null ? new Queue<PositionData>(positionData) : new Queue<PositionData>();
-        _cubeMovement.SetPositionQueue(positionQueue);
+        //var positionQueue = positionData != null ? new Queue<PositionData>(positionData) : new Queue<PositionData>();
+        //_cubeMovement.SetPositionQueue(positionQueue);
+
+        foreach (List<PositionData> cubeData in positionData){
+            GameObject cube = Instantiate(cubePrefab, new Vector3(0, -0.5f, 0), cubePrefab.transform.rotation);
+            var positionQueue = cubeData != null ? new Queue<PositionData>(cubeData) : new Queue<PositionData>();
+            cube.GetComponent<CubeMovement>().SetPositionQueue(positionQueue);
+        }
     }
 
     public void SetMovementEnabled(bool isMovementEnabled)
@@ -32,6 +40,11 @@ public class CubeMovementController : MonoBehaviour
             return;
         }
         
-        _cubeMovement.UpdatePosition();
+        GameObject[] cubes = GameObject.FindGameObjectsWithTag("Cube");
+        
+        foreach (GameObject cube in cubes){
+
+        cube.GetComponent<CubeMovement>().UpdatePosition();
+        }
     }
 }
